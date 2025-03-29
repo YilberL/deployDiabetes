33746c05a11eade90d1b819b5ae44221da66bab1
Unicomfacauca <unicomfacauca@PPN-MAC-PC24.local>
2025-03-29 11:46:05 -0500

    first commit

--- app3.py
+++ app3.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 import routerPredictions
 
 app = FastAPI()
 app.include_router(routerPredictions.router)
+
+app.add_middleware(
+    CORSMiddleware, # type: ignore
+    allow_origins=["*"],  # Puedes cambiar "*" por ["http://127.0.0.1:5500&quot;] si quieres más seguridad
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 if __name__=="__name__":
     app.run()
